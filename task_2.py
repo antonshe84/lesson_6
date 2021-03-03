390fbaade0a894ee13f2431fc2ed83b781f1cecd
antonshe84 <antonshe84@gmail.com>
2021-03-03 21:51:10 +0300

    final

--- task_2.py
+++ task_2.py
@@ -20,36 +20,37 @@
 которых превышает объем ОЗУ компьютера.
 """
 
-# Загрузка лога------
-import requests
 import sys
 
-# Код для получения файла по ссылке
+# ---Код для получения файла по ссылке
+import requests
 # site = "https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs"
 # data = requests.get(site)
 # s = data.text
 # data.close()
 # -------------------
 
-# Генератор большого файла
+# ---Генератор большого файла
 # for _ in range(1000):
 #     with open("nginx_logs.txt", "a", encoding="utf-8") as log_file:
 #         log_file.write(s)
 # -------------------
 
-
-lst = []
-ip_list = []
+# Парсим данные и записываем в файл на лету,
+# т.к. при большом рамере файла данных размер словаря может превысить объем ОЗУ
 with open("nginx_logs.txt", "r", encoding="utf-8") as log_file:
     with open("nginx_logs_parce.txt", "w", encoding="utf-8") as parce_file:
         while True:
             txt = log_file.readline()
-            if txt == '': break
+            if txt == '':
+                break
             st = txt.split()
             t1 = st[5].replace('"', '')
             parce_file.writelines(f"{st[0]} {t1} {st[6]}\n")
 print("Парсинг завершен, результат в файле 'nginx_logs_parce.txt'")
 
+# Из полученного файла nginx_logs_parce получаем словарь вида: {"уникальныq IP адрес": колличество его повторений}
+# читаем данные последовательно, чтобы не превысить объем ОЗУ
 with open("nginx_logs_parce.txt", "r", encoding="utf-8") as log_file:
     ip_count_dict = {}
     while True:
@@ -61,8 +62,8 @@ with open("nginx_logs_parce.txt", "r", encoding="utf-8") as log_file:
             ip_count_dict[v] = ip_count_dict.get(v) + 1
         else:
             ip_count_dict[v] = 1
-print("Подсчет колличесва запросов от IP завершен")
-print(f"Размер словаря уникальных IP адресов: {sys.getsizeof(ip_count_dict)}")
+print("Подсчет колличества запросов от IP завершен")
+print(f"Размер в байтах словаря уникальных IP адресов: {sys.getsizeof(ip_count_dict)}")
 
 # Если нужна сортировка словаря IP адресов по колличеству обращений:
 ip_count_dict = {k: ip_count_dict[k] for k in sorted(ip_count_dict, key=ip_count_dict.get, reverse=True)}
